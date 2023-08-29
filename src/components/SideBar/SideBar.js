ee3afb01aec097e395687ff6a47d34a6e83f7bc3
Dominick Harper <dominickdustinjames@gmail.com>
2023-08-29 08:54:47 -0600

    Refactor: SideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,7 +5,11 @@ const SideBar = ({ user }) => {
   return (
     <div className="sidebar">
       <div className="sidebar__user">
-        <img className="sidebar__avatar" alt="Avatar" />
+        <img
+          className="sidebar__avatar"
+          src="../../images/avatar.svg"
+          alt="Avatar"
+        />
         <h2 className="sidebar__username">{user.username}</h2>
       </div>
     </div>
